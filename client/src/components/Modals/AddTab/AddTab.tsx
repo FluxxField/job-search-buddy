9fa14c5375981278e378d91054895ca6d27cb271
Keenan Johns <keenanjj13@gmail.com>
2020-04-22 13:35:22 -0500

    Added buttons to AddTab model

--- client/src/components/Modals/AddTab/AddTab.tsx
+++ client/src/components/Modals/AddTab/AddTab.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useEffect, useRef } from "react";
 import Portal from "../../Portal/Portal";
+import Submit from "../../Inputs/Submit/Submit";
 import styles from "./AddTab.sass";
 
 const AddTab = ({ getNode }) => {
@@ -14,7 +15,21 @@ const AddTab = ({ getNode }) => {
       <Portal id={"root-modal"}>
         <div className={styles.wrapper} ref={node}>
           <div className={styles.box}>
-            <h1>AddTab</h1>
+            <form>
+              <label>Would you like to upload a file?</label>
+              <Submit
+                value={"Yes"}
+                onClick={() => {}}
+                error={false}
+                errorText={""}
+              />
+              <Submit
+                value={"No"}
+                onClick={() => {}}
+                error={false}
+                errorText={""}
+              />
+            </form>
           </div>
         </div>
       </Portal>
